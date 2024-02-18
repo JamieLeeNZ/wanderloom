df73feb0cdedd9b48abcc9236bf3aeab6a3f1755
Jamie Lee <126450738+eejl773@users.noreply.github.com>
2024-02-18 15:36:27 +1300

    feat: now uses closest attractions to each selected attraction to generate an itinerary

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,13 @@ import { Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import Random from "./pages/Random";
 import Select from "./pages/Select";
+import Itinerary from "./pages/Itinerary";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Select />} />
-
+      <Route path="/itinerary" element={<Itinerary />} />
     </Routes>
   )
 }
